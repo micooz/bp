dde82a165e7f39a11ae9e22778361badfdc9b4ba
micooz <micooz@hotmail.com>
2021-10-26 20:06:11 +0800

    fix: escaped chars in echo

--- build/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOCKER_IMAGE="rust:1.55.0-buster"
-TARGET_NAME="bp"
-WORK_DIR="/root/${TARGET_NAME}"
-
-docker pull ${DOCKER_IMAGE}
-docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":${WORK_DIR} -w ${WORK_DIR} ${DOCKER_IMAGE} cargo build --release
