d77eede69caa1c4c676a815c9d31e0bb7e00692d
micooz <micooz@hotmail.com>
2021-10-31 20:14:31 +0800

    feat: use rolling file for log4rs

--- bp-cli/src/logging.rs
+++ bp-cli/src/logging.rs
@@ -1,25 +1,36 @@
 use crate::dirs::Dirs;
 use log::LevelFilter;
-use log4rs::append::console::ConsoleAppender;
-use log4rs::append::file::FileAppender;
-use log4rs::config::{Appender, Config, Root};
-use log4rs::encode::pattern::PatternEncoder;
+use log4rs::{
+    append::{
+        console::ConsoleAppender,
+        rolling_file::{
+            policy::compound::{roll::fixed_window::FixedWindowRoller, trigger::size::SizeTrigger, CompoundPolicy},
+            RollingFileAppender,
+        },
+    },
+    config::{Appender, Config, Root},
+    encode::pattern::PatternEncoder,
+};
 
 pub fn init() {
     let encoder = Box::new(PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S%.3f)} {h({l})} [{M}] {m}{n}"));
     let console = ConsoleAppender::builder().encoder(encoder.clone()).build();
-    let file = FileAppender::builder()
+
+    let policy_trigger = SizeTrigger::new(10 * 1024 * 1024); // 10 MB
+    let policy_roller = FixedWindowRoller::builder().build("bp.{}.log", 5).unwrap();
+    let compound_policy = CompoundPolicy::new(Box::new(policy_trigger), Box::new(policy_roller));
+    let rolling_file = RollingFileAppender::builder()
         .encoder(encoder)
-        .build(Dirs::log_file())
+        .build(Dirs::log_file(), Box::new(compound_policy))
         .unwrap();
 
     let builder = Config::builder()
         .appender(Appender::builder().build("console", Box::new(console)))
-        .appender(Appender::builder().build("file", Box::new(file)));
+        .appender(Appender::builder().build("rolling_file", Box::new(rolling_file)));
 
     let root = Root::builder()
         .appender("console")
-        .appender("file")
+        .appender("rolling_file")
         .build(LevelFilter::Info);
 
     let config = builder.build(root).unwrap();
