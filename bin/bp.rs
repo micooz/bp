369be4718498a827154ce9b659f527dfb1e8fe36
micooz <micooz@hotmail.com>
2021-11-09 15:43:53 +0800

    chore: add dhat for heap profiling

--- bin/bp.rs
+++ bin/bp.rs
@@ -5,8 +5,15 @@ use bp_core::{check_options, Options, StartupInfo};
 use clap::Parser;
 use tokio::sync::oneshot;
 
+#[cfg(feature = "profile")]
+#[global_allocator]
+static ALLOCATOR: dhat::DhatAlloc = dhat::DhatAlloc;
+
 #[tokio::main]
 async fn main() {
+    #[cfg(feature = "profile")]
+    dhat::Dhat::start_heap_profiling();
+
     #[cfg(feature = "logging")]
     logging::init();
 
